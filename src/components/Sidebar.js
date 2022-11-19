f28355176117d963da02c159a47cc91dc90d1bee
Mikael Josias <mikaeljosias2002@gmail.com>
2022-11-19 16:31:18 -0300

    feat: create and style sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,103 @@
+export default function Sidebar(){
+    return (
+        <div class="sidebar">
+          <div class="sidebar__perfil">
+            <img
+              src="./assets/imgs/catanacomics.png"
+              alt="Perfil Catana Comics"
+            />
+            <div>
+              <a href="#CatanaComics">catanacomics</a>
+              <p>Catana</p>
+            </div>
+          </div>
+
+          <div class="sidebar__sugestoes">
+            <div>
+              <p>Sugestões para você</p>
+              <a>Ver tudo</a>
+            </div>
+
+            <div class="sidebar__sugestoes__perfil">
+              <div class="sidebar__sugestoes__perfil__detalhes">
+                <img
+                  src="./assets/imgs/badvibesmemes.png"
+                  alt="Logo Bad vibe memes"
+                />
+                <div>
+                  <a href="#Badvibesmeme">bad.vibes.memes</a>
+                  <p>Segue você</p>
+                </div>
+              </div>
+
+              <a href="#Seguir">Seguir</a>
+            </div>
+
+            <div class="sidebar__sugestoes__perfil">
+              <div class="sidebar__sugestoes__perfil__detalhes">
+                <img src="./assets/imgs/chibirdart.png" alt="Logo chibirdart" />
+                <div>
+                  <a href="#Badvibesmeme">chibirdart</a>
+                  <p>Segue você</p>
+                </div>
+              </div>
+
+              <a href="#Seguir">Seguir</a>
+            </div>
+
+            <div class="sidebar__sugestoes__perfil">
+              <div class="sidebar__sugestoes__perfil__detalhes">
+                <img
+                  src="./assets/imgs/razoesparaacreditar.png"
+                  alt="Logo razões para acreditar"
+                />
+                <div>
+                  <a href="#Badvibesmeme">razoesparaacreditar</a>
+                  <p>Novo no Instagram</p>
+                </div>
+              </div>
+
+              <a href="#Seguir">Seguir</a>
+            </div>
+
+            <div class="sidebar__sugestoes__perfil">
+              <div class="sidebar__sugestoes__perfil__detalhes">
+                <img
+                  src="./assets/imgs/adorableanimals.png"
+                  alt="Logo adorable animals"
+                />
+                <div>
+                  <a href="#Badvibesmeme">adorable_animals</a>
+                  <p>Segue você</p>
+                </div>
+              </div>
+
+              <a href="#Seguir">Seguir</a>
+            </div>
+
+            <div class="sidebar__sugestoes__perfil">
+              <div class="sidebar__sugestoes__perfil__detalhes">
+                <img
+                  src="./assets/imgs/smallcutecats.png"
+                  alt="Logo small cute cats"
+                />
+                <div>
+                  <a href="#Badvibesmeme">smallcutecats</a>
+                  <p>Segue você</p>
+                </div>
+              </div>
+
+              <a href="#Seguir">Seguir</a>
+            </div>
+          </div>
+
+          <div class="sidebar__sobre">
+            <p>
+              Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos
+              • Localizações • Contas mais relevantes • Hashtags • Idioma
+            </p>
+            <p>© 2021 INSTAGRAM DO FACEBOOK</p>
+          </div>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import reactDom from "react-dom";
 import Header from "./components/Header";
 import Stories from "./components/Stories";
 import Posts from "./components/Posts";
+import Sidebar from "./components/Sidebar";
 
 const root = document.querySelector(".root");
 
@@ -16,7 +17,7 @@ function Cabecalho() {
           <Posts />
         </div>
         <div className="conteudo__secundario">
-
+            <Sidebar/>
         </div>
       </div>
     </>
