7c721b2b5a5ac30eaafbe5f39cd7e5fbabff9977
Mikael Josias <mikaeljosias2002@gmail.com>
2022-11-20 10:34:38 -0300

    refactor: create sugestions component

---
+++ src/components/Sugestoes.js
@@ -0,0 +1,79 @@
+export default function Sugestoes() {
+  return (
+    <div class="sidebar__sugestoes">
+      <div>
+        <p>Sugestões para você</p>
+        <a>Ver tudo</a>
+      </div>
+
+      <div class="sidebar__sugestoes__perfil">
+        <div class="sidebar__sugestoes__perfil__detalhes">
+          <img src="" alt="Logo Bad vibe memes" />
+          <div>
+            <a href="#Badvibesmeme"></a>
+            <p>Segue você</p>
+          </div>
+        </div>
+
+        <a href="#Seguir">Seguir</a>
+      </div>
+
+      <div class="sidebar__sugestoes__perfil">
+        <div class="sidebar__sugestoes__perfil__detalhes">
+          <img src="./assets/imgs/chibirdart.png" alt="Logo chibirdart" />
+          <div>
+            <a href="#Badvibesmeme">chibirdart</a>
+            <p>Segue você</p>
+          </div>
+        </div>
+
+        <a href="#Seguir">Seguir</a>
+      </div>
+
+      <div class="sidebar__sugestoes__perfil">
+        <div class="sidebar__sugestoes__perfil__detalhes">
+          <img
+            src="./assets/imgs/razoesparaacreditar.png"
+            alt="Logo razões para acreditar"
+          />
+          <div>
+            <a href="#Badvibesmeme">razoesparaacreditar</a>
+            <p>Novo no Instagram</p>
+          </div>
+        </div>
+
+        <a href="#Seguir">Seguir</a>
+      </div>
+
+      <div class="sidebar__sugestoes__perfil">
+        <div class="sidebar__sugestoes__perfil__detalhes">
+          <img
+            src="./assets/imgs/adorableanimals.png"
+            alt="Logo adorable animals"
+          />
+          <div>
+            <a href="#Badvibesmeme">adorable_animals</a>
+            <p>Segue você</p>
+          </div>
+        </div>
+
+        <a href="#Seguir">Seguir</a>
+      </div>
+
+      <div class="sidebar__sugestoes__perfil">
+        <div class="sidebar__sugestoes__perfil__detalhes">
+          <img
+            src="./assets/imgs/smallcutecats.png"
+            alt="Logo small cute cats"
+          />
+          <div>
+            <a href="#Badvibesmeme">smallcutecats</a>
+            <p>Segue você</p>
+          </div>
+        </div>
+
+        <a href="#Seguir">Seguir</a>
+      </div>
+    </div>
+  );
+}
